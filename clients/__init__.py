82aa4da33e89b8c03e07822f01354ee6415127ec
Winking-maniac <gkgim@yandex.ru>
2024-05-03 14:04:52 +0300

    All docstring added. All checks passed.

---
+++ clients/__init__.py
@@ -0,0 +1,1 @@
+"""Submodule for different clients of game."""
--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -1,9 +1,22 @@
-import enum
+"""Submodule for main logic implementation(servers).
+
+API for client-server interaction provided.
+"""
+
 import dataclasses
 import datetime
+import enum
 
 
 class Num(enum.IntEnum):
+    """Subtype of int.
+
+    Representing 3 entites:
+    1) Possible numbers in sudoku
+    2) Possible row indexes
+    3) Possible column indexes
+    """
+
     ONE = 1
     TWO = 2
     THREE = 3
@@ -17,37 +30,70 @@ class Num(enum.IntEnum):
 
 @dataclasses.dataclass
 class Pos:
+    """Type representing point on board."""
+
     x: Num
     y: Num
 
 
-class Session:
+class SudokuSession:
+    """Representation of one game.
+
+    Declares API for initializing the game,
+    getting information about board and making step.
+    """
+
     def __init__(self) -> None:
+        """Game initializer.
+
+        Computes random sudoku board, ready to game.
+        """
         raise NotImplementedError
 
     def undo(self) -> bool:
+        """Undo last turn.
+
+        Returns: true if the turn was successful, false otherwise
+        """
         raise NotImplementedError
 
     def redo(self) -> bool:
+        """Redo last turn.
+
+        Returns: true if the turn was successful, false otherwise
+        """
         raise NotImplementedError
 
     def set_num(self, pos: Pos, num: Num) -> bool:
+        """Set the point at 'pos' value to 'num'.
+
+        Returns: true if the turn was successful, false otherwise
+        """
         raise NotImplementedError
 
     def del_num(self, pos: Pos) -> bool:
+        """Unset the point at 'pos'.
+
+        Returns: true if the turn was successful, false otherwise
+        """
         raise NotImplementedError
 
     def get_errors(self) -> list[list[bool]]:
+        """Get matrix of errors on board."""
         raise NotImplementedError
 
     def get_initials(self) -> list[list[bool]]:
+        """Get matrix of initials on board."""
         raise NotImplementedError
 
-    def get_board(self) -> list[list[Num]]:
+    def get_board(self) -> list[list[Num | None]]:
+        """Get matrix of board values."""
         raise NotImplementedError
 
 
 class Difficulty(enum.Enum):
+    """Difficulty of game."""
+
     Easy = 0
     Medium = 1
     Hard = 2
@@ -55,26 +101,50 @@ class Difficulty(enum.Enum):
 
 @dataclasses.dataclass
 class Save:
+    """Representation of save metadata."""
+
     name: str
     timestamp: datetime.datetime
     difficulty: Difficulty
     session_filename: str
 
 
-class Game:
+class SudokuServer:
+    """Representation of sudoku server.
+
+    Implements all server-side logic except game managing itself.
+    """
+
     _saves_directory: str
 
     def __init__(self) -> None:
+        """Server initializer."""
         raise NotImplementedError
 
-    def generate_session(self, difficulty: Difficulty = Difficulty.Medium) -> Session:
+    def generate_session(self, difficulty: Difficulty = Difficulty.Medium) -> SudokuSession:
+        """Generate session.
+
+        Typically used in 'New Game'.
+        """
         raise NotImplementedError
 
-    def save_session(self, session: Session) -> None:
+    def save_session(self, session: SudokuSession) -> None:
+        """Save session for  further gaming.
+
+        Typically used in 'Save'.
+        """
         raise NotImplementedError
 
     def list_saves(self) -> list[Save]:
+        """List saves metadata for further loading.
+
+        Typically used in some kind of 'Continue' menu.
+        """
         raise NotImplementedError
 
-    def load_session(self, save: Save) -> Session:
+    def load_session(self, save: Save) -> SudokuSession:
+        """Load session from save.
+
+        Typically used in some kind of 'Continue' menu.
+        """
         raise NotImplementedError
