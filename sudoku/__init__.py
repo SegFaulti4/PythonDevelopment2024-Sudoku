c63d762adaa1e716c7ed0888166818c0fd734815
SegFaulti4 <makar.popov01@mail.ru>
2024-05-24 21:39:35 +0300

    Implemented session logic

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -203,40 +203,95 @@ class SudokuSession:
 
         Returns: true if the turn was successful, false otherwise
         """
-        raise NotImplementedError
+        if self.history.turn + len(self.history.boards) == 0:
+            return False
+        self.history.turn -= 1
+        return True
 
     def redo(self) -> bool:
         """Redo last turn.
 
         Returns: true if the turn was successful, false otherwise
         """
-        raise NotImplementedError
+        if self.history.turn == -1:
+            return False
+        self.history.turn += 1
+        return True
 
     def set_num(self, pos: Pos, num: Num) -> bool:
         """Set the point at 'pos' value to 'num'.
 
         Returns: true if the turn was successful, false otherwise
         """
-        raise NotImplementedError
+        if self.history.initial[pos.x - 1][pos.y - 1]:
+            return False
+        if self.history.turn != -1:
+            del self.history.boards[self.history.turn + 1:]
+            self.history.turn = -1
+        self.history.boards.append([
+            [self.history.boards[self.history.turn][row][col]
+             if pos.x - 1 != row or pos.y - 1 != col
+             else num
+             for col in range(9)] for row in range(9)
+        ])
+        return True
 
     def del_num(self, pos: Pos) -> bool:
         """Unset the point at 'pos'.
 
         Returns: true if the turn was successful, false otherwise
         """
-        raise NotImplementedError
+        if self.history.initial[pos.x - 1][pos.y - 1] \
+                or self.history.boards[self.history.turn][pos.x - 1][pos.y - 1] is None:
+            return False
+        if self.history.turn != -1:
+            del self.history.boards[self.history.turn + 1:]
+            self.history.turn = -1
+        self.history.boards.append([
+            [self.history.boards[self.history.turn][row][col]
+             if pos.x - 1 != row or pos.y - 1 != col
+             else None
+             for col in range(9)] for row in range(9)
+        ])
+        return True
 
-    def get_errors(self) -> list[list[bool]]:
+    def get_errors(self) -> BoardMask:
         """Get matrix of errors on board."""
-        raise NotImplementedError
+        errors = [[False for __ in range(9)] for __ in range(9)]
+        board = self.history.boards[-1]
+        # Check rows and cols
+        for i in range(9):
+            for j1, j2 in permutations(range(9), 2):
+                if board[i][j1] == board[i][j2]:
+                    errors[i][j1] = True
+                    errors[i][j2] = True
+                if board[j1][i] == board[j2][i]:
+                    errors[j1][i] = True
+                    errors[j2][i] = True
+        # Check boxes
+        for box in range(9):
+            for cell1, cell2 in permutations(range(9), 2):
+                r1: int = 3 * (box // 3) + cell1 // 3
+                r2: int = 3 * (box // 3) + cell2 // 3
+                c1: int = 3 * (box % 3) + cell1 % 3
+                c2: int = 3 * (box % 3) + cell2 % 3
+                if board[r1][c1] == board[r2][c2]:
+                    errors[r1][c1] = True
+                    errors[r2][c2] = True
+        # Mask errors for unset cells
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] is None:
+                    errors[r][c] = False
+        return errors
 
     def get_initials(self) -> list[list[bool]]:
         """Get matrix of initials on board."""
-        raise NotImplementedError
+        return self.history.initial.copy()
 
     def get_board(self) -> list[list[Num | None]]:
         """Get matrix of board values."""
-        raise NotImplementedError
+        return self.history.boards[self.history.turn].copy()
 
 
 class SudokuServer:
