279c2133f6a7a4ad180614a352ab09972e896ff8
Winking-maniac <gkgim@yandex.ru>
2024-05-28 16:30:12 +0300

    Server file splitted

---
+++ sudoku/server.py
@@ -0,0 +1,113 @@
+"""Submodule for main logic implementation(servers).
+
+API for client-server interaction provided.
+"""
+
+import datetime
+import pathlib
+import uuid
+from random import getrandbits
+from random import seed as randseed
+
+import attrs
+
+import sudoku.algorithm as alg
+from sudoku.session import SudokuSession
+from sudoku.types import Board, Difficulty, SessionHistory, SessionSave
+
+
+class SudokuServer:
+    """Representation of sudoku server.
+
+    Implements all server-side logic except game managing itself.
+    """
+
+    _config_directory: pathlib.Path = pathlib.Path.home() / ".config" / "cmc_sudoku_2024"
+    _saves_directory: pathlib.Path = _config_directory / "saves"
+
+    def __init__(self) -> None:
+        """Server initializer."""
+        self._config_directory.mkdir(parents=True, exist_ok=True)
+        self._saves_directory.mkdir(parents=True, exist_ok=True)
+        randseed()
+
+    def _session_history_path(self, session_id: str) -> pathlib.Path:
+        return self._saves_directory / (session_id + "-history.json")
+
+    def _session_save_path(self, session_id: str) -> pathlib.Path:
+        return self._saves_directory / (session_id + "-save.json")
+
+    def _save_session_as_file(self, session: SudokuSession) -> None:
+        sid = session.save.session_id
+        # noinspection PyProtectedMember
+        session.history._save_as_file(self._session_history_path(sid))
+        # noinspection PyProtectedMember
+        session.save._save_as_file(self._session_save_path(sid))
+
+    def generate_session(self,
+                         name: str,
+                         seed: str | None = None,
+                         difficulty: Difficulty = Difficulty.Medium) -> SudokuSession:
+        """Generate session.
+
+        Typically used in 'New Game'.
+        """
+        history = SudokuServer._generate_session_history(seed=seed, difficulty=difficulty)
+        if seed is None:
+            seed = alg.calculate_seed(history.full_board, history.initial)
+        save = SessionSave(name=name, session_id=str(uuid.UUID(int=int(getrandbits(128)), version=4)), seed=seed,
+                           difficulty=difficulty, starting_field=history.boards[0])
+        session = SudokuSession(save, history)
+        self._save_session_as_file(session)
+        return session
+
+    def save_session(self, session: SudokuSession) -> None:
+        """Save session for further gaming.
+
+        Typically used in 'Save'.
+        """
+        kwargs = attrs.asdict(session.save) | {"timestamp": datetime.datetime.now().isoformat()}
+        new_save = SessionSave(**kwargs)
+        session.save = new_save
+        self._save_session_as_file(session)
+
+    def list_saves(self) -> list[SessionSave]:
+        """List saves metadata for further loading.
+
+        Typically used in some kind of 'Continue' menu.
+        """
+        saves: list[SessionSave] = []
+        for save_file in self._saves_directory.glob("*-save.json"):
+            # noinspection PyProtectedMember
+            save = SessionSave._from_file(save_file)
+            saves.append(save)
+        return saves
+
+    def delete_save(self, save: SessionSave) -> None:
+        """Delete save.
+
+        Completely deletes save. It cannot be restored in any way.
+        """
+        self._session_history_path(save.session_id).unlink()
+        self._session_save_path(save.session_id).unlink()
+
+    def load_session(self, save: SessionSave) -> SudokuSession:
+        """Load session from save.
+
+        Typically used in some kind of 'Continue' menu.
+        """
+        path = self._session_history_path(save.session_id)
+        # noinspection PyProtectedMember
+        history = SessionHistory._from_file(path)
+        session = SudokuSession(save=save, history=history)
+        return session
+
+    @staticmethod
+    def _generate_session_history(seed: str | None, difficulty: Difficulty) -> SessionHistory:
+        full_board = alg.generate_full_board(seed)
+        initial = alg.generate_initial_mask(full_board, seed=seed, difficulty=difficulty)
+        boards: list[Board] = [[[full_board[r][c] if initial[r][c] else None for c in range(9)] for r in range(9)]]
+        turn = -1
+
+        history = SessionHistory(full_board, initial, boards, turn)
+        return history
---
+++ sudoku/session.py
@@ -0,0 +1,131 @@
+"""Main game representation."""
+from itertools import permutations
+
+from sudoku.types import BoardMask, Num, Pos, SessionHistory, SessionSave
+
+
+class SudokuSession:
+    """Representation of one game.
+
+    Declares API for initializing the game,
+    getting information about board and making step.
+    """
+
+    save: SessionSave
+    history: SessionHistory
+    win_flag: bool
+
+    def __init__(self, save: SessionSave, history: SessionHistory) -> None:
+        """Game initializer.
+
+        Computes random sudoku board, ready to game.
+        """
+        self.save = save
+        self.history = history
+        self.win_flag = False
+
+    def win(self) -> bool:
+        """Check is the game ended.
+
+        If True, no operation on session is valid except win()
+        """
+        return self.win_flag
+
+    def undo(self) -> bool:
+        """Undo last turn.
+
+        Returns: true if the turn was successful, false otherwise
+        """
+        if self.win_flag or self.history.turn + len(self.history.boards) == 0:
+            return False
+        self.history.turn -= 1
+        return True
+
+    def redo(self) -> bool:
+        """Redo last turn.
+
+        Returns: true if the turn was successful, false otherwise
+        """
+        if self.win_flag or self.history.turn == -1:
+            return False
+        self.history.turn += 1
+        return True
+
+    def set_num(self, pos: Pos, num: Num) -> bool:
+        """Set the point at 'pos' value to 'num'.
+
+        Returns: true if the turn was successful, false otherwise
+        """
+        if self.win_flag or self.history.initial[pos.x - 1][pos.y - 1]:
+            return False
+        if self.history.turn != -1:
+            del self.history.boards[self.history.turn + 1:]
+            self.history.turn = -1
+        self.history.boards.append([
+            [self.history.boards[self.history.turn][row][col]
+             if pos.x - 1 != row or pos.y - 1 != col
+             else num
+             for col in range(9)] for row in range(9)
+        ])
+        if self.history.boards[self.history.turn] == self.history.full_board:
+            self.win_flag = True
+        return True
+
+    def del_num(self, pos: Pos) -> bool:
+        """Unset the point at 'pos'.
+
+        Returns: true if the turn was successful, false otherwise
+        """
+        if self.win_flag or self.history.initial[pos.x - 1][pos.y - 1] \
+                or self.history.boards[self.history.turn][pos.x - 1][pos.y - 1] is None:
+            return False
+        if self.history.turn != -1:
+            del self.history.boards[self.history.turn + 1:]
+            self.history.turn = -1
+        self.history.boards.append([
+            [self.history.boards[self.history.turn][row][col]
+             if pos.x - 1 != row or pos.y - 1 != col
+             else None
+             for col in range(9)] for row in range(9)
+        ])
+        return True
+
+    def get_errors(self) -> BoardMask:
+        """Get matrix of errors on board."""
+        errors = [[False for __ in range(9)] for __ in range(9)]
+        if self.win_flag:
+            return errors
+        board = self.history.boards[-1]
+        # Check rows and cols
+        for i in range(9):
+            for j1, j2 in permutations(range(9), 2):
+                if board[i][j1] == board[i][j2]:
+                    errors[i][j1] = True
+                    errors[i][j2] = True
+                if board[j1][i] == board[j2][i]:
+                    errors[j1][i] = True
+                    errors[j2][i] = True
+        # Check boxes
+        for box in range(9):
+            for cell1, cell2 in permutations(range(9), 2):
+                r1: int = 3 * (box // 3) + cell1 // 3
+                r2: int = 3 * (box // 3) + cell2 // 3
+                c1: int = 3 * (box % 3) + cell1 % 3
+                c2: int = 3 * (box % 3) + cell2 % 3
+                if board[r1][c1] == board[r2][c2]:
+                    errors[r1][c1] = True
+                    errors[r2][c2] = True
+        # Mask errors for unset cells
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] is None:
+                    errors[r][c] = False
+        return errors
+
+    def get_initials(self) -> list[list[bool]]:
+        """Get matrix of initials on board."""
+        return self.history.initial.copy()
+
+    def get_board(self) -> list[list[Num | None]]:
+        """Get matrix of board values."""
+        return self.history.boards[self.history.turn].copy()
